b137dfecd8aa0fd550e56808bde40c125513fa79
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2022-05-25 12:59:43 +1200

    Comment out sample tests

--- json-complete-clone/src/test/java/JsonExampleTest.java
+++ json-complete-clone/src/test/java/JsonExampleTest.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-
-public class JsonExampleTest {
-
-    @Test
-    public void personIsMick() {
-        assertEquals("Mick",JsonExample.createJSON().get("name"));
-    }
-}
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//
+//
+//public class JsonExampleTest {
+//
+//    @Test
+//    public void personIsMick() {
+//        assertEquals("Mick",JsonExample.createJSON().get("name"));
+//    }
+//}
