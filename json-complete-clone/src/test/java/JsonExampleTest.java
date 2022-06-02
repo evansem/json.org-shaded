86727c2e4e20fe00e73971197cc177545ca582ec
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2022-06-03 09:08:06 +1200

    Add stub dependency to clones
    This is to allow synk to correctly load the project

--- json-complete-clone/src/test/java/JsonExampleTest.java
+++ json-complete-clone/src/test/java/JsonExampleTest.java
@@ -1,11 +1,11 @@
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//
-//
-//public class JsonExampleTest {
-//
-//    @Test
-//    public void personIsMick() {
-//        assertEquals("Mick",JsonExample.createJSON().get("name"));
-//    }
-//}
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonExampleTest {
+
+    @Test
+    public void personIsMick() {
+        assertEquals("Mick",JsonExample.createJSON().get("name"));
+    }
+}
